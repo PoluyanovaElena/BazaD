d3373db6361cbcea9812742e96cc5cc1ee144210
PoluyanovaElena <167449123+PoluyanovaElena@users.noreply.github.com>
2024-11-01 21:49:20 +0500

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, status, Body, HTTPException
+from pydantic import BaseModel
+from typing import List
+app = FastAPI()
+users = []
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+@app.get("/users")
+async def get_users() -> List[User]:
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def post_user(username: str, age: int):
+    user_id = len(users) + 1
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return {f"User {user_id} is registered"}
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int, username: str, age: int):
+    try:
+        users[user_id] = f"Имя: {username}, возраст: {age}"
+        return f"The user {user_id} has been updated"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    try:
+        del users[user_id]
+        return f"User {user_id} has been deleted"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")

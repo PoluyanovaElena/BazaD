fa462289147646b46ce03379dc181da9d60b36cc
PoluyanovaElena <167449123+PoluyanovaElena@users.noreply.github.com>
2024-11-08 18:53:12 +0500

    Add files via upload

--- module_16_5.py
+++ module_16_5.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Body, HTTPException, Request
+from fastapi import FastAPI, status, Body, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List
@@ -23,7 +23,7 @@ def get_all_users(request: Request) -> HTMLResponse:
 @app.get(path="/user/{user_id}")
 async def get_users(request: Request, user_id: int) -> HTMLResponse:
     try:
-        return templates.TemplateResponse("users.html", {"request": request, "user": users[user_id]})
+        return templates.TemplateResponse("users.html", {"request": request, "user": users[user_id-1]})
     except IndexError:
         raise HTTPException(status_code=404, detail="User not found")
 
